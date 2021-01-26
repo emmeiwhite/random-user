5cf56c4eed614474cc4990c83235ae58ccc2cf30
Imran Rafiq Rather <emmeiwhite@gmail.com>
2021-01-26 16:24:00 +0530

    Initial JSX setup

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+  FaCreativeCommonsNcJp,
+} from "react-icons/fa";
+const url = "https://randomuser.me/api/";
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch(url);
+  }, []);
+  return (
+    <main className="main-wrapper">
+      <article className="user-details">
+        <img src={defaultImage} className="user-Image" />
+        <p>My name is</p>
+        <h3>Default Name</h3>
+        <div>
+          <FaUser />
+          <FaEnvelopeOpen />
+          <FaCreativeCommonsNcJp />
+          <FaLock />
+          <FaMap />
+          <FaPhone />
+          <FaCalendarTimes />
+        </div>
+      </article>
+    </main>
+  );
+}
+
+export default App;
